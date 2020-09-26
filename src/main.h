2fd419e8f4502fbbea5949f70da4b1b71048bee6
Essaulov Andrew <essaulovaa@mail.ru>
2020-09-26 13:37:53 +0300

    plot movement + Grid

--- src/main.h
+++ src/main.h
@@ -1,6 +1,7 @@
 #include "app.h"
 
 #define MAX_POINTS_COUNT 1024
+#define UNIT 10
 
 typedef struct app_s
 {
@@ -9,6 +10,8 @@ typedef struct app_s
 
     double iteration_step; // accuracy of line. Less value -> more point -> more smooth
     double user_value;     // user value to modify plot function
+
+    Vector2 shift;
 
     int points_count;
     Vector2 points[MAX_POINTS_COUNT];
@@ -23,8 +26,12 @@ void Init(app_t *app, int w, int h, double user_start)
 {
     app->width = w;
     app->height = h;
+
     app->user_value = user_start;
     app->iteration_step = iterationCalc(app->user_value);
+
+    app->shift.x = w / 2;
+    app->shift.y = h / 2;
 
     app->points_count = 0;
 }
@@ -35,18 +42,38 @@ void Input(app_t *app)
     {
         if (app->points_count < MAX_POINTS_COUNT)
         {
-            app->user_value += 10;
+            app->user_value += UNIT;
             app->iteration_step = iterationCalc(app->user_value);
         }
     }
 
     if (IsKeyPressed(KEY_KP_SUBTRACT))
     {
-        if (app->points_count > 10)
+        if (app->points_count > UNIT)
         {
             app->user_value -= 5;
             app->iteration_step = iterationCalc(app->user_value);
         }
+    }
+
+    if (IsKeyPressed(KEY_UP))
+    {
+        app->shift.y += UNIT;
+    }
+
+    if (IsKeyPressed(KEY_DOWN))
+    {
+        app->shift.y -= UNIT;
+    }
+
+    if (IsKeyPressed(KEY_RIGHT))
+    {
+        app->shift.x -= UNIT;
+    }
+
+    if (IsKeyPressed(KEY_LEFT))
+    {
+        app->shift.x += UNIT;
     }
 }
 
@@ -61,8 +88,8 @@ void Update(app_t *app)
 
         if (app->points_count <= MAX_POINTS_COUNT - 1)
         {
-            app->points[app->points_count].x = x + (GetScreenWidth() / 2);
-            app->points[app->points_count].y = y + (GetScreenHeight() / 2);
+            app->points[app->points_count].x = x + app->shift.x;
+            app->points[app->points_count].y = y + app->shift.y;
             app->points_count++;
         }
     }
@@ -75,27 +102,32 @@ void Render(app_t *app)
     BeginDrawing();
     ClearBackground(RAYWHITE);
 
-    int x_center = app->width / 2;
-    int y_center = app->height / 2;
+    DrawLine(0, app->shift.y, app->width, app->shift.y, BLUE);  // AXIS X
+    DrawLine(app->shift.x, 0, app->shift.x, app->height, BLUE); // AXIS Y
 
-    DrawLine(0, y_center, app->width, y_center, BLUE);  // AXIS X
-    DrawLine(x_center, 0, x_center, app->height, BLUE); // AXIS Y
-
-    for (int x = 0; x < app->width; x += 10)
+    for (int x = 0; x < app->width; x += UNIT)
     {
-        DrawLine(x, y_center - 3, x, y_center + 3, BLUE);
+        if ((x / 10) % 2  == 0)
+        {
+            DrawLineEx((Vector2){x, 0}, (Vector2){x, app->height}, 1.0f, LIGHTGRAY); // Grid
+        }
+        DrawLine(x, app->shift.y - 3, x, app->shift.y + 3, BLUE); // Measure
     }
 
-    for (int y = 0; y < app->height; y += 10)
+    for (int y = 0; y < app->height; y += UNIT)
     {
-        DrawLine(x_center - 3, y, x_center + 3, y, BLUE);
+        if ((y / 10) % 2 == 0)
+        {
+            DrawLineEx((Vector2){0, y}, (Vector2){app->width, y}, 1.0f, LIGHTGRAY); // Grid
+        }
+        DrawLine(app->shift.x - 3, y, app->shift.x + 3, y, BLUE); // Measure
     }
 
     DrawLineStrip(app->points, app->points_count, RED); // Plot
 
     char str[16];
     sprintf(str, "Points - %d", app->points_count);
-    DrawText(str, 10, 40, 18, DARKGREEN);
+    DrawText(str, UNIT, 40, 18, DARKGREEN);
 
     DrawFPS(10, 10);
     EndDrawing();
